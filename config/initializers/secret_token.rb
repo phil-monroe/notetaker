4a64ec2ea6094efbe7dfaa32a29b2821f8ac8450
Phil Monroe <phil@philmonroe.com>
2011-12-13 01:24:03 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notetaker::Application.config.secret_token = 'ff22df96fda1eaeb86ab7c2bff3fbce7745ad05c1dde7c424cf5803f618be3ca426c67d437b3dfcab6d88f2e50f3c64739e8a39974b8a6bd62a43fdee0b805b5'
