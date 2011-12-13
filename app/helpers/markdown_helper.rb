16b53d3fb542a632b9a3dfe63324c254e62cdc69
Phil Monroe <phil@philmonroe.com>
2011-12-13 12:16:13 -0800

    Styling

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -2,6 +2,6 @@ module MarkdownHelper
   def markdown(text)
     options = {hard_wrap: true, filter_html: true, autolink: true, no_intraemphasis: true, fenced_code: true, gh_blockcode: true, safe_links_only: true}
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
-    "<section class='markdown'>#{markdown.render(text)}</section>".html_safe
+    markdown.render(text).html_safe
   end
 end
